a8bcf49b97398b080ff982fc9a75ab544ca988b1
Geoff Nelson <geoffreysnelson@gmail.com>
2020-06-12 10:42:46 -0700

    setup aoo for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //Setup static content location
 app.use(express.static(path.join(__dirname, '../public')))
 
@@ -92,7 +94,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
